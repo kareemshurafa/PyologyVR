01525256b19d58af0781a8d9a2eef918a08233a6
kareemshurafa <kareemshurafa@gmail.com>
2023-11-26 18:11:00 +0000

    Setup example capsule object and added button features
    Removed all the previous objects in the Scene and added a capsule object to use as an example for the model of the heart.
    
    Added in 3 buttons that are attached to the camera - functionality within the script has not been added yet.

---
+++ Assets/Scripts/Button1Script.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button1Script : MonoBehaviour
+{
+    public float scale = .01f;
+    public Button Button1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
